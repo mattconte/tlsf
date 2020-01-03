4b176d7c6024811f583820aa4f98301c110144d9
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2020-01-03 14:41:23 +0500

    Add TLSF_API and tlsf_printf.
    Needed for static building.

--- tlsf.h
+++ tlsf.h
@@ -40,6 +40,12 @@
 
 #include <stddef.h>
 
+/* Definition of the TLSF_API. */
+/* Provide the ability to override linkage features of the interface. */
+#if !defined(TLSF_API)
+#define TLSF_API
+#endif
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -50,38 +56,38 @@ typedef void* tlsf_t;
 typedef void* pool_t;
 
 /* Create/destroy a memory pool. */
-tlsf_t tlsf_create(void* mem);
-tlsf_t tlsf_create_with_pool(void* mem, size_t bytes);
-void tlsf_destroy(tlsf_t tlsf);
-pool_t tlsf_get_pool(tlsf_t tlsf);
+TLSF_API tlsf_t tlsf_create(void* mem);
+TLSF_API tlsf_t tlsf_create_with_pool(void* mem, size_t bytes);
+TLSF_API void tlsf_destroy(tlsf_t tlsf);
+TLSF_API pool_t tlsf_get_pool(tlsf_t tlsf);
 
 /* Add/remove memory pools. */
-pool_t tlsf_add_pool(tlsf_t tlsf, void* mem, size_t bytes);
-void tlsf_remove_pool(tlsf_t tlsf, pool_t pool);
+TLSF_API pool_t tlsf_add_pool(tlsf_t tlsf, void* mem, size_t bytes);
+TLSF_API void tlsf_remove_pool(tlsf_t tlsf, pool_t pool);
 
 /* malloc/memalign/realloc/free replacements. */
-void* tlsf_malloc(tlsf_t tlsf, size_t bytes);
-void* tlsf_memalign(tlsf_t tlsf, size_t align, size_t bytes);
-void* tlsf_realloc(tlsf_t tlsf, void* ptr, size_t size);
-void tlsf_free(tlsf_t tlsf, void* ptr);
+TLSF_API void* tlsf_malloc(tlsf_t tlsf, size_t bytes);
+TLSF_API void* tlsf_memalign(tlsf_t tlsf, size_t align, size_t bytes);
+TLSF_API void* tlsf_realloc(tlsf_t tlsf, void* ptr, size_t size);
+TLSF_API void tlsf_free(tlsf_t tlsf, void* ptr);
 
 /* Returns internal block size, not original request size */
-size_t tlsf_block_size(void* ptr);
+TLSF_API size_t tlsf_block_size(void* ptr);
 
 /* Overheads/limits of internal structures. */
-size_t tlsf_size(void);
-size_t tlsf_align_size(void);
-size_t tlsf_block_size_min(void);
-size_t tlsf_block_size_max(void);
-size_t tlsf_pool_overhead(void);
-size_t tlsf_alloc_overhead(void);
+TLSF_API size_t tlsf_size(void);
+TLSF_API size_t tlsf_align_size(void);
+TLSF_API size_t tlsf_block_size_min(void);
+TLSF_API size_t tlsf_block_size_max(void);
+TLSF_API size_t tlsf_pool_overhead(void);
+TLSF_API size_t tlsf_alloc_overhead(void);
 
 /* Debugging. */
 typedef void (*tlsf_walker)(void* ptr, size_t size, int used, void* user);
-void tlsf_walk_pool(pool_t pool, tlsf_walker walker, void* user);
+TLSF_API void tlsf_walk_pool(pool_t pool, tlsf_walker walker, void* user);
 /* Returns nonzero if any internal consistency check fails. */
-int tlsf_check(tlsf_t tlsf);
-int tlsf_check_pool(pool_t pool);
+TLSF_API int tlsf_check(tlsf_t tlsf);
+TLSF_API int tlsf_check_pool(pool_t pool);
 
 #if defined(__cplusplus)
 };
